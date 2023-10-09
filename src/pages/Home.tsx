25856e4927746963c21c1d1a873cde2d68cf0b91
peetwerapat <werapatsaejong@gmail.com>
2023-10-09 14:11:52 +0700

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,15 @@ import useContent from '../hook/useContent'
 import classes from './Home.module.css'
 
 const Home = () => {
-  const { contents, isLoading } = useContent()
+  const { content, isLoading } = useContent()
 
   if (isLoading) return <h1>Loading...</h1>
 
   return (
     <div className={classes.container}>
       <div className="feed-container">
-        {contents &&
-          contents.map((content) => {
+        {content &&
+          content.map((content) => {
             return <Content key={content.id} content={content} />
           })}
       </div>
