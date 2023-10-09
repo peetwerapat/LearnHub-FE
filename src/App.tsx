6184756a1716427457c99ab856e0e157951392be
peetwerapat <werapatsaejong@gmail.com>
2023-10-09 13:29:14 +0700

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.css'
+import Navbar from './components/Navbar'
+// import Home from './pages/Home'
 
 function App() {
-  ;<div className="App"></div>
+  //   const { isLoggedIn } = useAuth()
+
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> */}
+    </div>
+  )
 }
 
 export default App
