73a83db5e4ebe93ffb4675e24a62de64d58bf83f
peetwerapat <werapatsaejong@gmail.com>
2023-10-09 13:53:08 +0700

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,22 @@
-const Home = () => {}
+import Content from '../components/Content'
+import useContent from '../hook/useContent'
+import classes from './Home.module.css'
+
+const Home = () => {
+  const { contents, isLoading } = useContent()
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  return (
+    <div className={classes.container}>
+      <div className="feed-container">
+        {contents &&
+          contents.map((content) => {
+            return <Content key={content.id} content={content} />
+          })}
+      </div>
+    </div>
+  )
+}
 
 export default Home
