2e6e3763ca3861205667f7eda7c2bceb865a1296
peetwerapat <werapatsaejong@gmail.com>
2023-10-11 14:38:00 +0700

    content

--- src/hook/useContent.tsx
+++ src/hook/useContent.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react'
 import { ContentDTO } from '../types/dto'
 import axios from 'axios'
 
-const useContent = () => {
+const useContent = (id: string) => {
   const [content, setContent] = useState<ContentDTO | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string>('')
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,9 +23,29 @@ const useContent = () => {
     }
 
     fetchData()
-  })
+  }, [id])
 
-  return { content, isLoading, error }
+  const deleteContent = async () => {
+    const token = localStorage.getItem('token')
+
+    setIsSubmitting(true)
+    try {
+      const res = await axios.delete(`https://api.learnhub.thanayut.in.th/content/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      console.log(res.data)
+    } catch (err) {
+      throw new Error('Cannot delete content')
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  return { content, isLoading, error, isSubmitting, deleteContent }
 }
 
 export default useContent
